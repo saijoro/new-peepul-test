7b614c17a9386ef2866e2db4465db4d2693720d1
fe-dev1 <fe-dev1@labsquire.com>
2025-03-22 08:15:01 +0530

    Update app config

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
-// app.config.ts
 import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   vite: {
@@ -8,7 +8,22 @@ export default defineConfig({
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
+      react(),
     ],
+    optimizeDeps: {
+      exclude: ["lucide-react"], // Ensure it’s not unnecessarily pre-bundled
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+        },
+      },
+    },
   },
   server: {
     preset: "vercel",
